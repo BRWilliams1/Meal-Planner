94612f4d026ddd233aa77442c3f40f8690cac41a
Anita Sally Sugumar <anitashally@gmail.com>
2023-09-28 14:17:14 -0400

    backend on household

--- java/src/main/java/com/techelevator/model/Households.java
+++ java/src/main/java/com/techelevator/model/Households.java
@@ -1,4 +1,23 @@
 package com.techelevator.model;
 
 public class Households {
+
+    private int householdId;
+    private String householdName;
+
+    public int getHouseholdId() {
+        return householdId;
+    }
+
+    public void setHouseholdId(int householdId) {
+        this.householdId = householdId;
+    }
+
+    public String getHouseholdName() {
+        return householdName;
+    }
+
+    public void setHouseholdName(String householdName) {
+        this.householdName = householdName;
+    }
 }
